3dbd4fc088b4a9878da5048b663e6827cb06c70d
EPOOOOOO <116776421+EPOOOOOO@users.noreply.github.com>
2026-04-07 16:56:55 +0800

    Update ActivitySync.py

--- ActivitySync.py
+++ ActivitySync.py
@@ -8,7 +8,7 @@ from Crypto.Cipher import PKCS1_v1_5
 import garth
 import zipfile
 import hashlib
-      
+        
 def encrpt(password, public_key):
     rsa = RSA.importKey(public_key)
     cipher = PKCS1_v1_5.new(rsa)
